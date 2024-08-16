fde13ab05782c2039628347c43216e3aa6cae65a
Isaic Gonzalez <go.isaic@gmail.com>
2024-08-15 22:54:46 -0500

    :Update: config"

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 3000,
   MONGODB_URI:
     process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/NewsExplorer",
   JWT_SECRET: process.env.JWT_SECRET || "auth code",
